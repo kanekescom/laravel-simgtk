842fbec815e492071b96a987e5143b2d1c06bd9e
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2024-01-20 14:42:24 +0700

    Replace using ->disabledOn('edit')

--- src/Filament/Resources/PensiunResource.php
+++ src/Filament/Resources/PensiunResource.php
@@ -52,35 +52,35 @@ class PensiunResource extends Resource
                             ->schema([
                                 Forms\Components\TextInput::make('gelar_depan')
                                     ->maxLength(255)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Gelar Depan'),
                                 Forms\Components\TextInput::make('nama')
                                     ->maxLength(255)
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Nama'),
                                 Forms\Components\TextInput::make('gelar_belakang')
                                     ->maxLength(255)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Gelar Belakang'),
                                 Forms\Components\TextInput::make('nik')
                                     ->numeric()
                                     ->length(16)
                                     ->unique(ignoreRecord: true)
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('NIK'),
                                 Forms\Components\TextInput::make('nuptk')
                                     ->numeric()
                                     ->length(16)
                                     ->unique(ignoreRecord: true)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('NUPTK'),
                                 Forms\Components\TextInput::make('nip')
                                     ->numeric()
                                     ->length(18)
                                     ->unique(ignoreRecord: true)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('NIP'),
                                 Forms\Components\Select::make('gender_kode')
                                     ->options(GenderEnum::class)
@@ -90,25 +90,25 @@ class PensiunResource extends Resource
                                     ->searchable()
                                     ->preload()
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Gender'),
                                 Forms\Components\TextInput::make('tempat_lahir')
                                     ->maxLength(255)
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Tempat Lahir'),
                                 Forms\Components\DatePicker::make('tanggal_lahir')
                                     ->date()
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Tanggal Lahir'),
                                 Forms\Components\TextInput::make('nomor_hp')
                                     ->tel()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Nomor HP'),
                                 Forms\Components\TextInput::make('email')
                                     ->email()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Email'),
                                 Forms\Components\Select::make('jenjang_pendidikan_kode')
                                     ->options(JenjangPendidikanEnum::class)
@@ -118,7 +118,7 @@ class PensiunResource extends Resource
                                     ->searchable()
                                     ->preload()
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Jenjang Pendidikan'),
                             ])->columns(3),
                         Tabs\Tab::make('Kepegawaian')
@@ -131,56 +131,56 @@ class PensiunResource extends Resource
                                     ->searchable()
                                     ->preload()
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Status Kepegawaian'),
                                 Forms\Components\TextInput::make('masa_kerja_tahun')
                                     ->numeric()
                                     ->minValue(0)
                                     ->maxValue(50)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Masa Kerja Tahun'),
                                 Forms\Components\TextInput::make('masa_kerja_bulan')
                                     ->numeric()
                                     ->minValue(0)
                                     ->maxValue(12)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Masa Kerja Bulan'),
                             ])->columns(3),
                         Tabs\Tab::make('CPNS')
                             ->schema([
                                 Forms\Components\TextInput::make('nomor_sk_cpns')
                                     ->maxLength(255)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Nomor SK CPNS'),
                                 Forms\Components\DatePicker::make('tmt_cpns')
                                     ->date()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('TMT CPNS'),
                                 Forms\Components\DatePicker::make('tanggal_sk_cpns')
                                     ->date()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Tanggal SK CPNS'),
                             ])->columns(3),
                         Tabs\Tab::make('PNS')
                             ->schema([
                                 Forms\Components\TextInput::make('nomor_sk_pns')
                                     ->maxLength(255)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Nomor SK PNS'),
                                 Forms\Components\DatePicker::make('tmt_pns')
                                     ->date()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('TMT PNS'),
                                 Forms\Components\DatePicker::make('tanggal_sk_pns')
                                     ->date()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Tanggal SK PNS'),
                             ])->columns(3),
                         Tabs\Tab::make('Pangkat')
                             ->schema([
                                 Forms\Components\TextInput::make('nomor_sk_pangkat')
                                     ->maxLength(255)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Nomor SK Pangkat'),
                                 Forms\Components\Select::make('golongan_kode')
                                     ->options(GolonganAsnEnum::class)
@@ -189,15 +189,15 @@ class PensiunResource extends Resource
                                     )->pluck('value'))
                                     ->searchable()
                                     ->preload()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Golongan'),
                                 Forms\Components\DatePicker::make('tmt_pangkat')
                                     ->date()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('TMT Pangkat'),
                                 Forms\Components\DatePicker::make('tanggal_sk_pangkat')
                                     ->date()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Tanggal SK Pangkat'),
                             ])->columns(2),
                         Tabs\Tab::make('Jabatan')
@@ -210,7 +210,7 @@ class PensiunResource extends Resource
                                     ->preload()
                                     ->required()
                                     ->columnSpanFull()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Sekolah'),
                                 Forms\Components\Select::make('status_tugas_kode')
                                     ->options(StatusTugasEnum::class)
@@ -220,7 +220,7 @@ class PensiunResource extends Resource
                                     ->searchable()
                                     ->preload()
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Status Tugas'),
                                 Forms\Components\Select::make('jenis_ptk_id')
                                     ->relationship('jenisPtk', 'nama')
@@ -228,37 +228,37 @@ class PensiunResource extends Resource
                                     ->searchable()
                                     ->preload()
                                     ->required()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Jenis PTK'),
                                 Forms\Components\Select::make('bidang_studi_pendidikan_id')
                                     ->relationship('bidangStudiPendidikan', 'nama')
                                     ->exists(table: BidangStudiPendidikan::class, column: 'id')
                                     ->searchable()
                                     ->preload()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Bidang Studi Pendidikan'),
                                 Forms\Components\Select::make('bidang_studi_sertifikasi_id')
                                     ->relationship('bidangStudiSertifikasi', 'nama')
                                     ->exists(table: BidangStudiSertifikasi::class, column: 'id')
                                     ->searchable()
                                     ->preload()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Bidang Studi Sertifikasi'),
                                 Forms\Components\Select::make('mata_pelajaran_id')
                                     ->relationship('mataPelajaran', 'nama')
                                     ->exists(table: MataPelajaran::class, column: 'id')
                                     ->searchable()
                                     ->preload()
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Mata Pelajaran'),
                                 Forms\Components\TextInput::make('jam_mengajar_perminggu')
                                     ->numeric()
                                     ->minValue(0)
                                     ->maxValue(50)
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Jam Mengajar Perminggu'),
                                 Forms\Components\Toggle::make('is_kepsek')
-                                    ->disabled()
+                                    ->disabledOn('edit')
                                     ->label('Kepsek'),
                             ])->columns(2),
                         Tabs\Tab::make('Pensiun')
