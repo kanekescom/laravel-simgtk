a366f0a9da0304a6ffb7357df2132116f7b8e6b1
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2024-02-17 18:28:32 +0700

    Add import button to Sekolah

--- src/Filament/Resources/SekolahResource/Pages/ListSekolah.php
+++ src/Filament/Resources/SekolahResource/Pages/ListSekolah.php
@@ -2,16 +2,13 @@
 
 namespace Kanekescom\Simgtk\Filament\Resources\SekolahResource\Pages;
 
+use EightyNine\ExcelImport\ExcelImportAction;
 use Filament\Actions;
 use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Kanekescom\Simgtk\Filament\Resources\SekolahResource;
+use Kanekescom\Simgtk\Imports\SekolahRombelImport;
 use Kanekescom\Simgtk\Models\JenjangSekolah;
-use Konnco\FilamentImport\Actions\ImportAction;
-use Konnco\FilamentImport\Actions\ImportField;
-use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
-use pxlrbt\FilamentExcel\Columns\Column;
-use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListSekolah extends ListRecords
 {
@@ -22,51 +19,11 @@ class ListSekolah extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            // ImportAction::make()
-            //     ->uniqueField('id')
-            //     ->fields([
-            //         ImportField::make('id')
-            //             ->rules('required|max:255')
-            //             ->label('ID'),
-            //         ImportField::make('nama')
-            //             ->rules('required|length:255')
-            //             ->label('Nama'),
-            //         ImportField::make('npsn')
-            //             ->rules('required|length:8')
-            //             ->label('NPSN'),
-            //         ImportField::make('jenjangSekolah.nama')
-            //             ->rules('required|max:255')
-            //             ->label('Jenjang Sekolah'),
-            //         ImportField::make('wilayah.nama')
-            //             ->rules('required|max:255')
-            //             ->label('Wilayah'),
-            //         ImportField::make('jumlah_kelas')
-            //             ->rules('required|max:255')
-            //             ->label('Jumlah Kelas'),
-            //         ImportField::make('jumlah_rombel')
-            //             ->rules('required|max:255')
-            //             ->label('Jumlah Rombel'),
-            //         ImportField::make('jumlah_siswa')
-            //             ->rules('required|max:255')
-            //             ->label('Jumlah Siswa'),
-            //     ]),
-            // ExportAction::make()
-            //     ->exports([
-            //         ExcelExport::make()
-            //             ->withFilename(fn ($resource) => str($resource::getSlug())->replace('/', '_') . '-' . now()->format('Y-m-d'))
-            //             ->withWriterType(\Maatwebsite\Excel\Excel::XLSX)
-            //             ->withColumns([
-            //                 Column::make('id')->heading('ID'),
-            //                 Column::make('nama')->heading('Nama'),
-            //                 Column::make('npsn')->heading('NPSN'),
-            //                 Column::make('jenjangSekolah.nama')->heading('Jenjang'),
-            //                 Column::make('wilayah.nama')->heading('Wilayah'),
-            //                 Column::make('jumlah_kelas')->heading('Jumlah Kelas'),
-            //                 Column::make('jumlah_rombel')->heading('Jumlah Rombel'),
-            //                 Column::make('jumlah_siswa')->heading('Jumlah Siswa'),
-            //             ])
-            //             ->ignoreFormatting(),
-            //     ])->icon(false),
+            ExcelImportAction::make()
+                ->use(SekolahRombelImport::class)
+                ->slideOver()
+                ->icon(false)
+                ->label('Update Rombel'),
             Actions\CreateAction::make()->label('Create'),
         ];
     }
